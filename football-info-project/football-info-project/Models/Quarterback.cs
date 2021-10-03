bee98e5c111296354b0faa2e564cd9d115bf5b99
Silvam2017 <msilva@aggienetwork.com>
2021-10-03 16:07:56 -0400

    Created Quarterback Class with initial first statistic.

---
+++ football-info-project/football-info-project/Models/Quarterback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace football_info_project.Models
+{
+    public class Quarterback
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Tag helper to set display name across the entire app.
+        [DisplayName("Name")]
+        [Required]
+        public string Name { get; set; }
+
+        //First QB statistic = Completion Percentage. 
+        [Required]
+        [Range(0,100, ErrorMessage ="Completion % can only be between 0 and 100%.")]
+        public int Compl { get; set; }
+    }
+}
